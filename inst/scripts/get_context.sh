874a522c7b45f60eb8e85ece26c2bd871e696c0c
Shantao Li <shantaolee@gmail.com>
2020-07-16 22:38:25 -0700

    update the script to take chr[0-9X] as chr names

--- inst/scripts/get_context.sh
+++ inst/scripts/get_context.sh
@@ -6,14 +6,27 @@ CONTEXT_LEN="$3"
 BEDTOOLS="$4"
 REF_GENOME="$5"
 
+# check the chromosome format in REF_GENOME
+chr_name=`head -1 $REF_GENOME | cut -f1`
+
+
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	VCF=`echo $line | awk -F"[ |\t|,]" '{print $1}'`
 	SAMPLE=`echo $line | awk -F"[ |\t|,]" '{if (NF > 1) {print $2} \
 			else {gsub(".*\/",""); print $1}}'`
-	grep  -v "#" $VCF | sed "s/^chr//g" | awk -v var=$CONTEXT_LEN \
-			'{OFS="\t"; print $1, $2-1-var, $2+var}' | \
-			$BEDTOOLS getfasta -fi $REF_GENOME -bed - | \
-			grep -v "^>" | awk '{print toupper($0)}' > $OUTPUT".tmp"
+    if [[ $chr_name == *"chr"* ]]
+    then
+	    grep  -v "#" $VCF | awk -v var=$CONTEXT_LEN \
+		    	'{OFS="\t"; print $1, $2-1-var, $2+var}' | \
+			    $BEDTOOLS getfasta -fi $REF_GENOME -bed - | \
+			    grep -v "^>" | awk '{print toupper($0)}' > $OUTPUT".tmp"
+    else
+	    grep  -v "#" $VCF | sed "s/^chr//g" | awk -v var=$CONTEXT_LEN \
+		    	'{OFS="\t"; print $1, $2-1-var, $2+var}' | \
+			    $BEDTOOLS getfasta -fi $REF_GENOME -bed - | \
+			    grep -v "^>" | awk '{print toupper($0)}' > $OUTPUT".tmp"
+        
+    fi 
 	grep  -v "#" $VCF | awk -v var=$SAMPLE '{print $5"\t"var}' | \
 			paste -d"\t" $OUTPUT".tmp" - >>  $OUTPUT
 	rm $OUTPUT".tmp"
